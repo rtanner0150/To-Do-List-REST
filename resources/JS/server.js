e59257d1005390a4ee58546e19125d708a130a2b
Jeffery Kimbrow <71730781+kimbrow-slice@users.noreply.github.com>
2020-12-03 13:26:32 -0600

    updated server.js

--- resources/JS/server.js
+++ resources/JS/server.js
@@ -1,21 +1,68 @@
-//const is just another let
-//we declare a constant variable called mongoose
-//we set it equal to the return value of require('mongoose');
-const mongoose = require('mongoose'); 
-const { stringify } = require('querystring');
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://WebDevAdmin:124512AXEL@mycluster.0lxio.mongodb.net/to-do-list-db?retryWrites=true&w=majority'; 
 
-//access the connect method of the mongoose object
-//pass in the localhost test database
-//and some options inside of another object
-mongoose.connect('mongodb+srv://WebDevAdmin:%4*6#uhE98nFRR$@mycluster.0lxio.mongodb.net/RESTfulAPI?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+    if(err) return console.error(err);
+    console.log('Connected to database'); 
+}); 
 
-//make another constant variable called db
-//adn we set it equal to connection property of our mongoose object
 const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
-//but then we access the on and once methods of our connection property from our mongoose object
-db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
+    //your tutorial and new code go here. 
+    console.log("We're connected");
+    //schema    
+    const itemSchema = new mongoose.Schema({
+        item: [{
+             itemName : String,
+             itemNumber : Number,
+             assignee : String,
+             status : Boolean
+         }]
+     });
+    const listSchema = new mongoose.Schema({
+        name : String,
+        item : [itemSchema]
+    });
+
+
+    //
+    listSchema.methods.title = function() {
+        let intro;
+        if (this.name){
+            intro = "This is the " + this.name;
+        } else {
+            intro = "Please name your list";
+        }
+        console.log(intro);
+    }
+    //model
+    const Tasks = mongoose.model('Tasks', listSchema);
+    //documents 
+    const homework= new Tasks ({name: "Homework"});
+    console.log(homework.name);
+    const chores = new Tasks ({name : "Chores"});
+
+    homework.title();
+    chores.title();
+
+    //save after creating/ updating
+    homework.save(function(err, task){
+        if (err) return console.error(err);
+        task.title();
+    });
+    chores.save(function(err, task){
+        if (err) return console.error(err);
+        task.title();
+    });
+    //static method- attached directly to our model/class
+
+   Tasks.find(function(err, tasks){
+        if (err) return console.error(err);
+        console.log(tasks);
+    })
+
 
 
 });
