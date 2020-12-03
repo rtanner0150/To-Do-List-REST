23a174867eaa64e6b83e807c32957140226b4278
Jeffery Kimbrow <71730781+kimbrow-slice@users.noreply.github.com>
2020-12-03 09:41:39 -0600

    Section1 completed

---
+++ resources/JS/server.js
@@ -0,0 +1,21 @@
+//const is just another let
+//we declare a constant variable called mongoose
+//we set it equal to the return value of require('mongoose');
+const mongoose = require('mongoose'); 
+const { stringify } = require('querystring');
+
+//access the connect method of the mongoose object
+//pass in the localhost test database
+//and some options inside of another object
+mongoose.connect('mongodb+srv://WebDevAdmin:<password>@mycluster.0lxio.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//make another constant variable called db
+//adn we set it equal to connection property of our mongoose object
+const db = mongoose.connection;
+
+//but then we access the on and once methods of our connection property from our mongoose object
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+
+
+});
